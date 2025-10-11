22f17ff6c75cbe6e45e75f8b350be7a78a5e1d48
vivek <vivan100123@gmail.com>
2025-10-11 18:33:52 +0530

    Favicon updated

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+        {/* <link rel="alternate icon" href="/favicon.ico" /> */}
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
